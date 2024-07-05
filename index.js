88e668ef7e28547840f474faa68d73d6157a7177
Vitor-Linaris <linaris@DESKTOP-D7AD2JN>
2024-07-05 01:59:35 -0300

    feat: add route to delete a task

--- index.js
+++ index.js
@@ -33,6 +33,24 @@ app.post("/tasks", async (req, res) => {
     }
 });
 
+app.delete("/tasks/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+
+        const taskToDelete = await TaskModel.findById(taskId);
+
+        if (!taskToDelete) {
+            return res.status(500).send("Essa tarefa não foi encontrada");
+        }
+
+        const deletedTask = await TaskModel.findByIdAndDelete(taskId);
+
+        res.status(200).send(deletedTask);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
 const PORT = 8000;
 app.listen(PORT, () => {
     console.log(`Listening on port: http://localhost:${PORT}/`);
